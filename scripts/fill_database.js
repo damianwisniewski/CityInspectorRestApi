b00780db6364652377723a6ccd7887e6c9347627
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-07-21 11:36:14 +0200

    db scripts change

--- scripts/fill_database.js
+++ scripts/fill_database.js
@@ -1,6 +1,5 @@
 const faker = require('faker/locale/pl')
-
-const { sequelize, models } = require('../app/models')
+const { models } = require('../app/models')
 
 /**
 * Pushes users data to database
@@ -139,17 +138,6 @@ const createCommentsData = (usersWithComments, notificationsWithComments) => {
  */
 (async () => {
 	try {
-		/**
-		 * ['FOREIGN_KEY_CHECKS']
-		 * [1] - Enables foreign
-		 * [0] - Disables foreign
-		 * Foreign key constraints (database relations - associations)
-		 *
-		 * It's needed to disable key constraint for sync in force mode
-		 */
-		await sequelize.query('SET FOREIGN_KEY_CHECKS=0;')
-		await sequelize.sync({ force: true })
-
 		const users = await createUserData()
 
 		users.forEach(async (user) => {
@@ -164,9 +152,7 @@ const createCommentsData = (usersWithComments, notificationsWithComments) => {
 			await createCommentsData(usersWithComments, notificationsWithComments)
 		})
 
-		await sequelize.query('SET FOREIGN_KEY_CHECKS=1;')
 	} catch (err) {
 		console.log(err)
-		await sequelize.query('SET FOREIGN_KEY_CHECKS=1;')
 	}
 })()
---
+++ scripts/sync_database.js
@@ -0,0 +1,23 @@
+const { sequelize } = require('../app/models')
+const forced = process.argv.includes('--force')
+
+const init = async () => {
+	try {
+		/**
+		 * ['FOREIGN_KEY_CHECKS']
+		 * [1] - Enables foreign
+		 * [0] - Disables foreign
+		 * Foreign key constraints (database relations - associations)
+		 *
+			 * It's needed to disable key constraint for sync in force mode
+			 */
+		await sequelize.query('SET FOREIGN_KEY_CHECKS=0;')
+		await sequelize.sync({ force: forced })
+		await sequelize.query('SET FOREIGN_KEY_CHECKS=1;')
+	} catch (err) {
+		console.log(err)
+		await sequelize.query('SET FOREIGN_KEY_CHECKS=1;')
+	}
+}
+
+init()
