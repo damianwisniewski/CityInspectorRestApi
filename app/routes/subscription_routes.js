68010f674584c5d36621361f4b6bb916fd4c8e90
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-09-01 23:20:53 +0200

    subscribtion validation

--- app/routes/subscription_routes.js
+++ app/routes/subscription_routes.js
@@ -1,14 +1,45 @@
+/* eslint-disable prettier/prettier */
 const { Router } = require('express')
 const subscriptionRouter = Router()
 
-const authService = require('../middlewares/auth_middleware')
-const { get, add, remove } = require('../controllers/subscription_controller')
+// middlewares
+const authMiddleware = require('../middlewares/auth_middleware')
+const validatorMiddleware = require('../middlewares/data_validation_middleware')
+
+const subscriptionController = require('../controllers/subscription_controller')
+
+/**
+ * @typedef {import('express-validator').Schema} Schema
+ */
+
+/**
+ * Collection of possible inputs, and its validation and sanitization rules.
+ * @type {Schema}
+ */
+const inputRules = {
+	notification: {
+		in: 'query',
+		exists: true,
+	},
+}
 
 /**
  * From path [/subscription]
  */
-subscriptionRouter.post('/', authService, add)
-subscriptionRouter.get(['/', '/:subscriptionId'], authService, get)
-subscriptionRouter.delete(['/', '/:subscriptionId'], authService, remove)
+subscriptionRouter.post('/',
+	validatorMiddleware(inputRules),
+	authMiddleware,
+	subscriptionController.add,
+)
+
+subscriptionRouter.get(['/', '/:subscriptionId'],
+	authMiddleware,
+	subscriptionController.get
+)
+
+subscriptionRouter.delete(['/', '/:subscriptionId'],
+	authMiddleware,
+	subscriptionController.remove
+)
 
 module.exports = subscriptionRouter
