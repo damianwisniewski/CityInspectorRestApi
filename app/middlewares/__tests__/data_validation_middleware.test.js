59c02218d38a2d080782f5b62318ecee99486ee1
Damian Wisniewski <damian.wisniewski@performgroup.com>
2019-09-01 23:02:49 +0200

    Validation middleware

--- app/middlewares/__tests__/data_validation_middleware.test.js
+++ app/middlewares/__tests__/data_validation_middleware.test.js
@@ -1,1 +1,61 @@
-// TODO: Validation middleware tests
+const { expect, ...chai } = require('chai')
+const sinonChai = require('sinon-chai')
+const sinon = require('sinon')
+
+const validator = require('express-validator')
+const validationMiddleware = require('../data_validation_middleware')
+
+chai.use(sinonChai)
+
+describe('Data validation middleware', () => {
+	const validationHander = validationMiddleware({})[1]
+
+	const req = {}
+	const res = {}
+
+	before(() => {
+		sinon.stub(validator, 'validationResult')
+	})
+
+	afterEach(() => sinon.reset())
+	after(() => sinon.restore())
+
+	it('should go to next middleware after validation without error', done => {
+		validator.validationResult.returns({
+			isEmpty: () => true,
+		})
+
+		const next = args => {
+			try {
+				expect(args).to.be.undefined
+				done()
+			} catch (err) {
+				done(err)
+			}
+		}
+
+		validationHander(req, res, next)
+	})
+
+	it('should go to next middleware after validation without error', done => {
+		validator.validationResult.returns({
+			isEmpty: () => false,
+			array: () => [
+				{
+					msg: 'mock value',
+				},
+			],
+		})
+
+		const next = args => {
+			try {
+				expect(args).to.have.keys('status', 'message')
+				done()
+			} catch (err) {
+				done(err)
+			}
+		}
+
+		validationHander(req, res, next)
+	})
+})
--- app/middlewares/data_validation_middleware.js
+++ app/middlewares/data_validation_middleware.js
@@ -1,1 +1,23 @@
-// TODO: Validation
+const validator = require('express-validator')
+
+/**
+ * Creates validation middleware.
+ * @param {import('express-validator').Schema} validationSchema
+ * @return {import('express').RequestHandler[]}
+ */
+module.exports = validationSchema => [
+	/**
+	 * express-validator middleware to validate passed schemas.
+	 */
+	validator.checkSchema(validationSchema),
+	/**
+	 * Middleware to handle validation result
+	 */
+	(req, res, next) => {
+		const errors = validator.validationResult(req)
+
+		errors.isEmpty()
+			? next()
+			: next({ status: 422, message: errors.array({ onlyFirstError: true })[0].msg })
+	},
+]
